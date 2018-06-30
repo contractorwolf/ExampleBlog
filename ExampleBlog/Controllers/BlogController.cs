c7d4a0d598166396e317e8601f9fa32a418a1c9f
Logan Ward <wardlogan@gmail.com>
2018-06-30 16:34:49 -0400

    First ExampleBlog commit

---
+++ ExampleBlog/Controllers/BlogController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExampleBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleBlog.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlogsController : Controller
+    {
+        List<Blog> blogList = PopulateBlogs();
+
+        [HttpGet]
+        public IActionResult ListBlogs()
+        {
+            ViewData["Header"] = "Select a blog";
+            ViewData["Blogs"] = blogList;
+            return View(blogList);
+        }
+
+        // GET api/blogs/5
+        [HttpGet("{id}")]
+        public Blog Get(int id)
+        {
+            foreach (Blog b in blogList)
+            {
+                if (b.id == id)
+                {
+                    return b;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return null;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public static List<Blog> PopulateBlogs()
+        {
+            List<Blog> blogList = new List<Blog>();
+            Blog logansBlog = new Blog();
+            Blog eileensBlog = new Blog();
+            Blog marksBlog = new Blog();
+            logansBlog.id = 1;
+            logansBlog.author = "Logan";
+            logansBlog.title = "Mazdas";
+            logansBlog.body = "Mazdas are super cool and you should buy one";
+            eileensBlog.id = 2;
+            eileensBlog.author = "Eileen";
+            eileensBlog.title = "Dogs";
+            eileensBlog.body = "We don't have enough dogs and we should get more";
+            marksBlog.id = 3;
+            marksBlog.author = "Mark";
+            marksBlog.title = "Shoes are awesome";
+            marksBlog.body = "I spent too much on shoes today";
+            blogList.Add(logansBlog);
+            blogList.Add(eileensBlog);
+            blogList.Add(marksBlog);
+            return blogList;
+        }
+    }
+}
---
+++ ExampleBlog/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleBlog.Models
+{
+    public class Blog
+    {
+        public int id { get; set; }
+        public String author { get; set; }
+        public String title { get; set; }
+        public String body { get; set; }
+    }
+}
